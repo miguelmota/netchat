20341f8f8850bd370b8cf65ad8542fc3466e2fa5
Miguel Mota <miguelmota2@gmail.com>
2014-08-30 13:44:36 -0700

    initial

---
+++ README.md
@@ -0,0 +1,29 @@
+# Node Net Chat
+
+Chat client in Node.js
+
+# Install
+
+```bash
+git clone https://github.com/miguelmota/node-net-chat
+
+cd ndoe-net-chat
+
+npm install
+```
+
+## Running chat server
+
+```bash
+node index.js
+```
+
+## Connecting to chat server
+
+```
+telnet <server ip address> 9423
+```
+
+# License
+
+Released under the MIT License.
---
+++ index.js
@@ -0,0 +1,49 @@
+var net = require('net');
+
+var clients = [];
+
+var port = 9423;
+
+net.createServer(function(socket) {
+
+  var lineEntered = 0;
+
+  socket.name = socket.remoteAddress + ':' + socket.remotePort
+
+  socket.write('Welcome,' + socket.name + '\n');
+  socket.write('Type "quit" to exit.\n\n');
+  socket.write('Enter username: ');
+
+  socket.on('data', function (data) {
+    lineEntered += 1;
+    if (lineEntered === 1) {
+      socket.username = data.toString().trim();
+      clients.push(socket);
+      broadcast(socket.username + ' joined the chat\n', socket);
+    } else {
+      if (data.toString().trim() === 'quit') {
+        socket.end('Goodbye.\n');
+        return;
+      }
+      broadcast(socket.username + ': ' + data, socket);
+    }
+    socket.write('> ');
+  });
+
+  socket.on('end', function () {
+    clients.splice(clients.indexOf(socket), 1);
+    broadcast(socket.name + ' left.\n');
+  });
+
+  function broadcast(message, sender) {
+    clients.forEach(function (client) {
+      if (client === sender) return;
+      client.write(message);
+    });
+
+    console.log(message)
+  }
+
+}).listen(port);
+
+console.log('Server running on port ' + port + '\n');
