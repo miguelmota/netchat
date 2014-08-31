f2a3ee59a74becf4fd5bba7821c1912f09331006
Miguel Mota <miguelmota2@gmail.com>
2014-08-30 19:00:39 -0700

    add color

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 var net = require('net');
+var _ = require('lodash');
+var clc = require('cli-color');
+var EE = require('events').EventEmitter;
 
 var clients = [];
 
@@ -6,44 +9,80 @@ var port = 9423;
 
 net.createServer(function(socket) {
 
+  var ee = new EE();
   var lineEntered = 0;
 
   socket.name = socket.remoteAddress + ':' + socket.remotePort
 
-  socket.write('Welcome,' + socket.name + '\n');
+  socket.write(clc.green('Welcome, ' + socket.name + '\n'));
   socket.write('Type "quit" to exit.\n\n');
-  socket.write('Enter username: ');
+  socket.write(clc.yellow('Enter username: '));
 
   socket.on('data', function (data) {
     lineEntered += 1;
-    if (lineEntered === 1) {
-      socket.username = data.toString().trim();
-      clients.push(socket);
-      broadcast(socket.username + ' joined the chat\n', socket);
-    } else {
-      if (data.toString().trim() === 'quit') {
-        socket.end('Goodbye.\n');
-        return;
-      }
-      broadcast(socket.username + ': ' + data, socket);
+    var string = data.toString().trim();
+    if (string.charCodeAt() === 65533 || string === 'quit') {
+      ee.emit('quit');
+      return;
     }
-    socket.write('> ');
+    if (lineEntered === 1) {
+      socket.username = string || randUsername();
+      clients.push(socket);
+      socket.write(clc.green('You are now ' + socket.username) + '\n');
+      broadcast(JSON.stringify({data: socket.username + ' joined the chat'}), socket);
+    } else {
+      broadcast(JSON.stringify({username: socket.username, data: string}), socket);
+    }
+    writeCursor.call(socket);
+  });
+
+  ee.on('quit', function() {
+    socket.end('Goodbye.\n');
   });
 
   socket.on('end', function () {
     clients.splice(clients.indexOf(socket), 1);
-    broadcast(socket.name + ' left.\n');
+    broadcast(JSON.stringify({data: socket.username + ' left.'}), null, true);
   });
 
-  function broadcast(message, sender) {
-    clients.forEach(function (client) {
+  function broadcast(data, sender, alert) {
+    try {
+      data = JSON.parse(data)
+    } catch(e) {
+      throw e;
+    }
+    var message;
+    if (data.username) {
+      message = clc.cyan(data.username) + ': ' + clc.yellow(data.data);
+    } else {
+      if (alert)
+        message = clc.red(data.data);
+      else
+        message = clc.green(data.data);
+    }
+    _.each(clients, function(client) {
       if (client === sender) return;
-      client.write(message);
+      client.write(message + '\n');
+      writeCursor.call(client);
     });
 
-    console.log(message)
+    console.log(message);
+  }
+
+  function writeCursor() {
+    this.write(clc.magenta('> '));
+  }
+
+  function randUsername() {
+    return 'anonymous' + _.reduce(_.range(5), function(a, v, i) {
+      return (a += _.random(9), a);
+    }, '');
   }
 
 }).listen(port);
+
+process.on('SIGNINT', function() {
+  process.exit();
+});
 
 console.log('Server running on port ' + port + '\n');
